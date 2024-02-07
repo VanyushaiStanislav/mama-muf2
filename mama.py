f5b4e9a8c0a52b0bfd533d94c61e31bcc2e3782c
VanyushaiStanislav <159257259+VanyushaiStanislav@users.noreply.github.com>
2024-02-07 14:40:51 +0300

    Create mama.py

---
+++ mama.py
@@ -0,0 +1,33 @@
+#1
+k = int(input("Введите число k"))
+a = 100
+b = 92
+c = a**3-b**2+k
+print (c)
+#2 
+import math;
+a=int(input("Введите число a"))
+math.pow(c,0.5)
+c=math.pow((1 - math.cos(c)) / 2,0.5)
+print(c)
+#3
+from math import sin, cos, log, exp, pow
+v=int(input("Введите число v"))
+math.pow(v,0.5)
+v=(v*math.log(v,2)/(-5/(4*v+math.exp(v/2))))
+print(v)
+#4
+from math import sin, cos, pow
+c=int(input("Введите число c"))
+v=int(input("Введите число v"))
+math.pow(c,0.5)
+c = math.pow(math.pow(math.sin(c),2) + math.pow(math.cos(math.pow(v, 3)),2),0.5 )
+print(c)
+#5
+from math import pow 
+z=int(input("Введите число z"))
+f=int(input("Введите число f"))
+math.pow(z, 0.5)
+z = math.pow(z+math.pow(math.pow(z, 2) + 4* math.pow(f, 2), 0.5),0.5)
+print(z)
+#6
